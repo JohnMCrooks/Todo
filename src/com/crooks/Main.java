8588fac8b08bcb63dc4fd9872eb99f23ab532959
John Crooks <johnmcrooks@gmail.com>
2016-06-13 12:15:38 -0400

    Week 5 Day 1 - Added Database

--- src/com/crooks/Main.java
+++ src/com/crooks/Main.java
@@ -1,26 +1,32 @@
 package com.crooks;
 
+import org.h2.tools.Server;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void createItem(Scanner scanner, ArrayList<ToDoItem> items){
+    public static void createItem(Scanner scanner, Connection conn) throws SQLException {
         //Create new item
         System.out.println("Enter your ToDo Item");
         String text = scanner.nextLine();
-        ToDoItem item = new ToDoItem(false, text);
-        items.add(item);
+//        ToDoItem item = new ToDoItem(false, text);
+//        items.add(item);
+        insertTODO(conn, text);                             //Added SQL method isntead
+
     }
 
-    public static void toggleItem(Scanner scanner, ArrayList<ToDoItem> items){
+    public static void toggleItem(Scanner scanner, Connection conn) throws SQLException {
         //Toggle items state
         System.out.println("Enter Item number to toggle:\n");
         String numStr = scanner.nextLine();
         try {
             int num = Integer.valueOf(numStr);
-            ToDoItem tempItem = items.get(num - 1);
-            tempItem.isDone = !tempItem.isDone;
+//            ToDoItem tempItem = items.get(num - 1);
+//            tempItem.isDone = !tempItem.isDone;
+            toggleTODO(conn,num);                            //Added SQL method instead
         }catch (NumberFormatException e){
             System.out.println("You must use a number!\n");
         }catch (ArrayIndexOutOfBoundsException e){
@@ -28,23 +34,57 @@ public class Main {
         }
     }
 
-    public static void listItems(ArrayList<ToDoItem> items) {
+    public static void listItems(Connection conn) throws SQLException {
         //List items
-        int i = 1;
-
+//        int i = 1;
+        ArrayList<ToDoItem> items = selectTODO(conn);   //uses SQL method to populate items list.
         for (ToDoItem toDoItem: items) {
             String checkbox = "[ ]";
             if (toDoItem.isDone){
                 checkbox = "[x]";
             }
-            System.out.printf("%s %s - %s\n", checkbox, i,toDoItem.text);
-            i++;
-        }
+            System.out.printf("%s %s - %s\n", checkbox, toDoItem.id,toDoItem.text);
+//            i++;
+     }
     }
 
-    public static void main(String[] args) {
+    public static void insertTODO(Connection conn, String text) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO todos VALUES(NULL, ?, FALSE)");
+        stmt.setString(1, text);     //define first value and what you want it to be
+        stmt.execute();              //execute task
 
-        ArrayList<ToDoItem> items = new ArrayList<ToDoItem>();
+    }
+
+    public static ArrayList<ToDoItem> selectTODO(Connection conn) throws SQLException {      //Display all of the info in the table
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM todos");
+        ResultSet results = stmt.executeQuery();
+        ArrayList<ToDoItem> items = new ArrayList<>();
+        while(results.next()){                              //Looping through the results and populating the Array List
+            int id = results.getInt("id");
+            String text = results.getString("text");
+            boolean isDone = results.getBoolean("is_done");  //pulling all 3 item attributes
+            ToDoItem item = new ToDoItem(id,isDone,text);    //creating an object
+            items.add(item);                                //throwing object in array list
+        }
+        return items;                                       //   returning completed array list
+    }
+
+    public static void toggleTODO(Connection conn, int id) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement(" UPDATE todos SET is_done = NOT is_done WHERE id = ?");
+        stmt.setInt(1,id);
+        stmt.execute();
+    }
+
+    public static void main(String[] args) throws SQLException {
+
+        Server.createWebServer().start();
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+
+        stmt.execute("CREATE TABLE IF NOT EXISTS todos(id IDENTITY, text VARCHAR, is_done BOOLEAN)");  //Create table in database
+
+
         Scanner scanner = new Scanner(System.in);
 
         while(true){
@@ -56,15 +96,15 @@ public class Main {
 
             switch (option) {
                 case "1":
-                    createItem(scanner, items);
+                    createItem(scanner, conn);
                     break;
 
                 case "2":
-                    toggleItem(scanner, items);
+                    toggleItem(scanner, conn);
                     break;
 
                 case "3":
-                    listItems(items);
+                    listItems(conn);
                     break;
 
                 default:
--- src/com/crooks/ToDoItem.java
+++ src/com/crooks/ToDoItem.java
@@ -6,10 +6,13 @@ package com.crooks;
 public class ToDoItem {
     Boolean isDone;
     String text;
+    int id;
 
-    public ToDoItem(Boolean isDone, String text) {
+    public ToDoItem(int id, Boolean isDone, String text) {
+        this.id = id;
         this.isDone = isDone;
         this.text = text;
+
     }
 
 }
